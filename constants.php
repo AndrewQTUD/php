34c82579fbc8974c2add747a487cd27c65a82f25
Andrew Q <C15339351@mytudublin.ie>
2019-11-30 19:23:10 +0000

    Learning about constants

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+    //Vallid constant names
+    define("GREETINGS", "Hello world!");
+    define("GREETINGS2", "Hello you!");
+    define("GREETINGS_3", "Hello everyone");
+
+    define("2GREETING", "Hello");
+
+echo GREETINGS, '<br>';
+echo GREETINGS2, '<br>';
+echo GREETINGS_3, '<br>';
